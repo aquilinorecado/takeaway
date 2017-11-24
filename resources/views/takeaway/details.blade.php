d275df00185d09af2959fe236bb62545f38a771f
Stelio Tonica <srtstelio@yahoo.com.br>
2017-11-24 04:44:01 +0200

    updated take away edit

--- resources/views/takeaway/details.blade.php
+++ resources/views/takeaway/details.blade.php
@@ -15,24 +15,24 @@
                   <h1>Visualizar Tipo de Pagamento </h1>
               
               <div>
-                  <a href="{{ route('top.index') }}" class="label label-primary">Voltar</a>
+                  <a href="{{ route('takeaway.index') }}" class="label label-primary">Voltar</a>
               </div>
           </div>
           <div class="row">
             <div class="span10 offset1">
-                <form action="{{ route('top.insert') }}" method="POST" class="form-horizontal well">
+                <form action="{{ route('takeaway.insert') }}" method="POST" class="form-horizontal well">
                   <fieldset>
                     {{ csrf_field() }}
                     <div class="control-group">
                         <label class="control-label" >Descri&ccedil;&atilde;o :</label>
                        <div class="controls">
-                            <input type="text"  class="input-xlarge" value="{{ $top->description }}" readonly>
+                            <input type="text"  class="input-xlarge" value="{{ $takeaway->description }}" readonly>
                         </div>
                     </div>
                     <div class="control-group">
                         <label class="control-label" >Estado :</label>
                        <div class="controls">
-                            <input type="text"  class="input-xlarge" value="{{ $top->state }}" readonly>
+                            <input type="text"  class="input-xlarge" value="{{ $takeaway->state }}" readonly>
                         </div>
                     </div>
                     
